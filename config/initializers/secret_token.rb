690eac17e0eae3deeaaf12554a6e72ac722d5fce
Stefan Boesen <stefan.boesen@gmail.com>
2013-07-31 14:37:22 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EdurangeScoring::Application.config.secret_token = '0a3fbd77ffac61f6d0e0c0207cf0fca65c5797f88834a593e03c7ba44bfd66386946f5646c193747e486aebd618ed46ff1038706805f9cbdbcf9a984b279654b'
