ddd32f38d45f8dd6af7800c6311763d01dd75795
Stefan Boesen <stefan.boesen@gmail.com>
2014-02-28 03:26:01 -0800

    Added bunch of hacks to get users into scoring automatically

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -1,22 +1,5 @@
 class Scenario < ActiveRecord::Base
   attr_accessible :name, :nodes, :users
-  after_create :parse_data
 
   has_many :attempts
-
-  def parse_data
-    # Create users for every user
-    users = self.users.split('------ENDUSER------')
-    users.each do |user|
-      name,pass,priv = user.split(',')
-      name = name.sub(/^[\n\r]*/, '') # Remove start newlnes
-      if name.blank? || pass.blank? || priv.blank?
-        next
-      end
-      pass = pass.sub(/^[\n\r]*/, '') 
-      priv = priv.sub(/^[\n\r]*/, '')
-      user = User.find_or_create_by_email :name => name, :email => name + "@edurange.com", :password => pass, :password_confirmation => pass, :private_key => priv
-    end
-
-  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,13 @@ puts 'DEFAULT USERS'
 user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
 puts 'user: ' << user.name
 user.add_role :admin
+
+File.open(File.expand_path(File.dirname(__FILE__)) + '/users.txt', 'r').each_line do |line|
+  user, pass = line.split(',')
+  user = user.chomp
+  pass = pass.chomp
+  User.find_or_create_by_email :name => user.dup, :email => user.dup+"@edurange.org", :password => pass.dup, :password_confirmation => pass.dup
+end
+nodes = File.open(File.expand_path(File.dirname(__FILE__)) + '/nodes.txt', 'rb').read
+scenario = Scenario.new
+binding.pry
